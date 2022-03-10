f36193981d8392fa1b3ca098ec3f47cba93c6189
Jesus Alarcon <jesus.leo0508@gmail.com>
2022-03-10 08:42:11 -0500

    first commit

---
+++ src/main/java/pe/com/project1/ms/MsAccountApplication.java
@@ -0,0 +1,13 @@
+package pe.com.project1.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAccountApplication.class, args);
+	}
+
+}
